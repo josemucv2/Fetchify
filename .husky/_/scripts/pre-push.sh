275ce576f8be9b996b508bf1955c64f6efce5cd1
josemucv2 <martinezcaceresjosem@gmail.com>
2024-12-22 16:33:17 -0400

    fix

--- .husky/_/scripts/pre-push.sh
+++ .husky/_/scripts/pre-push.sh
@@ -5,6 +5,6 @@ current_branch=$(git symbolic-ref --short HEAD)
 
 # Verifica si la rama actual es 'main'
 if [ "$current_branch" = "main" ]; then
-  echo "¡Error! No se puede hacer push directo a la rama main."
+  echo "❌❌❌¡Error! No se puede hacer push directo a la rama main.❌❌❌"
   exit 1
 fi
