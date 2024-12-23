f2c055cf1a4aa713dc827bf1e6909dc5d24978b8
josemucv2 <martinezcaceresjosem@gmail.com>
2024-12-22 20:10:02 -0400

    fix: name principal

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,1 @@
-import { Fetchify } from "./core";
-
-export const FetchifyInscante = Fetchify
-
-
-const instasce = FetchifyInscante.create({ baseURL: '', headers: {} })
-
-instasce.setToken({ token: '' })
-
-instasce.setRequestInterceptor(async (config) => {
-    const token = localStorage.getItem("accessToken");
-    if (token) {
-        config.headers.set("Authorization", `Bearer ${token}`);
-    }
-    console.log("Request Interceptor:", config);
-    return config;
-});
-
+export { Fetchify } from "./core";
