27cb2b408f29862cfc02ee68413f5d4c880d9116
josemucv2 <martinezcaceresjosem@gmail.com>
2024-12-28 23:19:05 -0400

    fix: file index

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,1 @@
-import { Fetchify } from "./core";
-
 export { Fetchify } from "./core";
-
-
-const COALLY_API = Fetchify.create({ baseURL: 'https://coally-api-2.onrender.com/api/v1' })
-
-
-
-
-export const loginServices = async () => {
-    const data = await COALLY_API.POST({ endpoint: 'auth/login', body: { identifier: 'asda', password: 'asdasd' } })
-
-
-    return data
-}
-
-loginServices()
